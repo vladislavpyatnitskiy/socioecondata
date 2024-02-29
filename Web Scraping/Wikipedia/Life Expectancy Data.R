51509dba081875e5adb920c26b63801f0ed8aef2
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-02-29 22:21:41 +0500

    Create Life Expectancy Data.R
    Added script to get life expectancy data by countries from Wikipedia

---
+++ Web Scraping/Wikipedia/Life Expectancy Data.R
@@ -0,0 +1,22 @@
+library("rvest") # Library
+
+life.expectancy.df <- function(x){ # Data Frame of Countries & life expectancy
+  
+  s <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = ""))
+  
+  s.wiki <- s %>% html_nodes('table') %>% .[[2]] -> tab # Assign Table 
+  
+  y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
+  
+  v <- NULL # Form a Data Frame
+  
+  for (n in 0:(length(y)/16)){ v <- rbind(v, cbind(y[1+n*16], y[2+n*16])) }
+  
+  colnames(v) <- c("Country", "Life Expectancy") # Column Names
+  rownames(v) <- seq(nrow(v)) # Numbers as row names
+  
+  v <- v[apply(v, 1, function(x) all(!is.na(x))),] # Get rid of NA
+  
+  v # Display
+}
+life.expectancy.df("List_of_countries_by_life_expectancy")
