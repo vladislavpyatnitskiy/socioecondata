dad6f58da71799af0bdb1b10d9dab2569f1a07a7
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2024-10-14 16:02:32 +0500

    Update Canada Population Data.R
    Reduced excessive lines

--- Web Scraping/Wikipedia/Canada Population Data.R
+++ Web Scraping/Wikipedia/Canada Population Data.R
@@ -2,30 +2,14 @@ library("rvest") # Library
 
 population.ca <- function(x){ # Canadian provinces and their population
   
-  s <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = ""))
+  y <- read_html(paste("https://en.wikipedia.org/wiki/", x, sep = "")) %>%
+    html_nodes('table') %>% .[[1]] %>% html_nodes('tr') %>%
+    html_nodes('td') %>% html_text()
   
-  s.wiki <- s %>% html_nodes('table') %>% .[[1]] -> tab # Assign Table 
-  
-  y <- tab %>% html_nodes('tr') %>% html_nodes('td') %>% html_text()
-  
-  v <- NULL # variables to store data frames
-  
-  for (n in 0:(length(y) / 6)){ # Put data in columns
-    
-    d <- y[1 + n * 6] # Provinces names
-  
-    d <- read.fwf(textConnection(d), widths = c(nchar(1), nchar(d)),
-                  colClasses = "character")[2]
-    
-    D <- as.numeric(gsub(",", "", y[4 + n * 6])) # Population data
-    
-    v <- rbind.data.frame(v, data.frame(d, D)) } # Join data
+  v <- data.frame(y[seq(from=1,to=length(y),by=6)],
+                  as.numeric(gsub(",", "", y[seq(from=4,to=length(y),by=6)])))
   
   colnames(v) <- c("Province", "Population") # Column names
-  
-  v <- v[apply(v, 1, function(x) all(!is.na(x))),] # Get rid of NA
-  
-  rownames(v) <- seq(nrow(v)) # numbers instead of row names
   
   v # Display
 }
